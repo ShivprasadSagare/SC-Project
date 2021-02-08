60188b8d4a1101c917253076d708fd2200be6de9
ShivprasadSagare <shivprasad.sagare@gmail.com>
2021-02-08 17:05:34 +0530

    added try-except block and modified data file to include hindi sitelinks

--- data/fetch_triples.py
+++ data/fetch_triples.py
@@ -4,17 +4,18 @@ from collections import defaultdict
 import pickle
 import json
 import dill
+from tqdm import tqdm
 
 endpoint_url = "https://query.wikidata.org/sparql"
 
-query = """SELECT DISTINCT ?person ?personLabel ?article WHERE {
+query1 = """SELECT DISTINCT ?person ?personLabel ?article WHERE {
   ?person wdt:P641 wd:Q5375;
           wdt:P31 wd:Q5.
   ?article schema:about ?person;
            schema:isPartOf <https://hi.wikipedia.org/>.
   
   SERVICE wikibase:label { 
-    bd:serviceParam wikibase:language "hi". 
+    bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". 
   }
 }"""
 
@@ -26,31 +27,39 @@ def get_results(endpoint_url, query):
     sparql.setReturnFormat(JSON)
     return sparql.query().convert()
 
-results = get_results(endpoint_url, query)
+results1 = get_results(endpoint_url, query1)
 
 def extract_id(id):
     start = id.find('Q')
     return id[start:]
 
-ids = []
-for result in results['results']['bindings']:
-    ids.append((extract_id(result['person']['value']), result['personLabel']['value']))
-
-query = """SELECT ?propertyLabel ?objectLabel WHERE {
+query2 = """SELECT ?propertyLabel ?objectLabel WHERE {
   wd:%s ?predicate ?object.
   ?property wikibase:directClaim ?predicate.
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
 }
 """
+
 dict = defaultdict(lambda: [])
-for id in ids:
-    results = get_results(endpoint_url, query%id[0])
-    dict[id[0]] = {}
-    dict[id[0]]['personLabel'] = id[1]
-    dict[id[0]]['triples'] = [{'propertyLabel': result['propertyLabel']['value'], 'objectLabel': result['objectLabel']['value']} for result in results['results']['bindings']]
+success, fail = 0, 0
+fail_ids = []
+  
+with tqdm(results1['results']['bindings'], desc="fetching entity data", ) as pbar:
+  for result1 in pbar:
+    pbar.set_description_str("success: %d, fail: %d" % (success, fail))
+    Q_id = extract_id(result1['person']['value'])
+    try:
+      results2 = get_results(endpoint_url, query2%Q_id)
+      success += 1
+    except:
+      fail += 1
+      fail_ids.append(Q_id)
+      continue
+    dict[Q_id] = {}
+    dict[Q_id]['personLabel'] = result1['personLabel']['value']
+    dict[Q_id]['Hindi sitelink'] = result1['article']['value']
+    dict[Q_id]['triples'] = [{'propertyLabel': result2['propertyLabel']['value'], 'objectLabel': result2['objectLabel']['value']} for result2 in results2['results']['bindings']]
+
 
 with open('cricketers.json', 'w') as outfile:
     json.dump(dict, outfile)
-
-
-
