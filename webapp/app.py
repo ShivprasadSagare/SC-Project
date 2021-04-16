2624f02c7fe29d05fbbf66c59ffc6a915b1dace8
Vijay Vardhan <32735958+vijayvardhan9@users.noreply.github.com>
2021-04-17 01:10:28 +0530

    Web app updated

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 import hashlib
+from typing import Tuple
 from flask import Flask, session, url_for, redirect, render_template, request, abort, flash
 from werkzeug.utils import secure_filename
 import json
@@ -42,7 +43,7 @@ for key in politiciansDataFile.keys() :
     politiciansKeys.append(key)
 
 # global iterator
-politiciansIterator = 10
+politiciansIterator = 0
 @app.route("/politicians", methods=['GET', 'POST'])
 def FUN_politicians():
     if(request.method == 'POST'):
@@ -52,22 +53,26 @@ def FUN_politicians():
         print(req)
         print(sentence)
         global politiciansIterator
-        politiciansIterator += 1
         flag = False
 
         # Write into output
-        text = '{\n"entity_id" : "' + politiciansKeys[politiciansIterator] + '",\n"triples": [\n'
+        jsonobject = {politiciansKeys[politiciansIterator]:{"entity_id":politiciansKeys[politiciansIterator], "triples":[], "sentence":sentence}}
+        subject = politiciansDataFile[politiciansKeys[politiciansIterator]]['personLabel']
         for triple in req:
-            subject = politiciansDataFile[politiciansKeys[politiciansIterator]]['personLabel']
             predicate = triple.split(', ')[0]
             obj = triple.split(', ')[1]
+            temp_dict = {'subject':'', 'predicate':'', 'object':''}
             if predicate != '' and obj != '':
-                text += '{\n\t"subject": "' + subject + '",\n\t"predicate": "' + predicate + '",\n\t"object": "' + obj + '"\n},'
+                temp_dict['subject'] = subject
+                temp_dict['predicate'] = predicate
+                temp_dict['object'] = obj
                 flag = True
-        text = text[:-1]
-        text += '\n],\n"sentence": "' + sentence + '"\n},\n' 
-        if flag :
-            politiciansOutputFile.write(text)    
+            jsonobject[politiciansKeys[politiciansIterator]]['triples'].append(temp_dict)
+        if flag:
+            politiciansOutputFile.write('"'+ politiciansKeys[politiciansIterator] + '":')
+            json.dump(jsonobject[politiciansKeys[politiciansIterator]], politiciansOutputFile)
+            politiciansOutputFile.write(',\n')
+        politiciansIterator += 1
         
 
 
@@ -90,21 +95,25 @@ def FUN_cricketers():
         print(req)
         print(sentence)
         global cricketersIterator
-        cricketersIterator += 1
         flag = False
-
         # Write into output
-        text = '{\n"entity_id" : "' + cricketersKeys[cricketersIterator] + '",\n"triples": [\n'
+        jsonobject = {cricketersKeys[cricketersIterator]:{"entity_id":cricketersKeys[cricketersIterator], "triples":[], "sentence":sentence}}
+        subject = cricketersDataFile[cricketersKeys[cricketersIterator]]['personLabel']
         for triple in req:
-            subject = cricketersDataFile[cricketersKeys[cricketersIterator]]['personLabel']
             predicate = triple.split(', ')[0]
             obj = triple.split(', ')[1]
+            temp_dict = {'subject':'', 'predicate':'', 'object':''}
             if predicate != '' and obj != '':
-                text += '{\n\t"subject": "' + subject + '",\n\t"predicate": "' + predicate + '",\n\t"object": "' + obj + '"\n},\n'
+                temp_dict['subject'] = subject
+                temp_dict['predicate'] = predicate
+                temp_dict['object'] = obj
                 flag = True
-        text += '],\n"sentence": "' + sentence + '"\n},\n' 
+            jsonobject[cricketersKeys[cricketersIterator]]['triples'].append(temp_dict)
         if flag:
-            cricketersOutputFile.write(text)
+            cricketersOutputFile.write('"'+ cricketersKeys[cricketersIterator] + '":')
+            json.dump(jsonobject[cricketersKeys[cricketersIterator]], cricketersOutputFile)
+            cricketersOutputFile.write(',\n')
+        cricketersIterator += 1
 
     return render_template("cricketers.html", title="cricketers", jsonfile=cricketersDataFile, length = len(cricketersDataFile.keys()), key = cricketersKeys[cricketersIterator], iterator = cricketersIterator)
 
@@ -123,22 +132,103 @@ def FUN_actors():
         sentence = request.form.get('sentence')
         req = request.form.getlist('triples')
         global actorsIterator
-        actorsIterator += 1
         flag = False
 
         # Write into output
-        text = '{\n"entity_id" : "' + actorsKeys[actorsIterator] + '",\n"triples": [\n'
+        jsonobject = {actorsKeys[actorsIterator]:{"entity_id":actorsKeys[actorsIterator], "triples":[], "sentence":sentence}}
+        subject = actorsDataFile[actorsKeys[actorsIterator]]['personLabel']
         for triple in req:
-            subject = actorsDataFile[actorsKeys[actorsIterator]]['personLabel']
             predicate = triple.split(', ')[0]
             obj = triple.split(', ')[1]
-            text += '{\n\t"subject": "' + subject + '",\n\t"predicate": "' + predicate + '",\n\t"object": "' + obj + '"\n},\n'
-            flag = True
-        text += '],\n"sentence": "' + sentence + '"\n},\n' 
-        if flag :
-            actorsOutputFile.write(text)
+            temp_dict = {'subject':'', 'predicate':'', 'object':''}
+            if predicate != '' and obj != '':
+                temp_dict['subject'] = subject
+                temp_dict['predicate'] = predicate
+                temp_dict['object'] = obj
+                flag = True
+            jsonobject[actorsKeys[actorsIterator]]['triples'].append(temp_dict)
+        if flag:
+            actorsOutputFile.write('"'+ actorsKeys[actorsIterator] + '":')
+            json.dump(jsonobject[actorsKeys[actorsIterator]], actorsOutputFile)
+            actorsOutputFile.write(',\n')
+        actorsIterator += 1
 
     return render_template("actors.html", title="actors", jsonfile=actorsDataFile, length = len(actorsDataFile.keys()), key=actorsKeys[actorsIterator], iterator = actorsIterator)
+
+
+# TEST RESULTS
+
+politiciansTestFile = open('Transformer Output/politicians_test.json', 'r')
+politiciansResultsFile = open('Transformer Output/politicians_matches.json', 'r')
+with open('Transformer Output/politicians_test.json', 'r') as politiciansTestFile:
+    politiciansTestData = json.load(politiciansTestFile)
+with open('Transformer Output/politicians_matches.json', 'r') as politiciansResultsFile:
+    politiciansResultsData = json.load(politiciansResultsFile)
+
+politiciansTestKeys = []
+politiciansResultsKeys = []
+
+for key in politiciansTestData.keys():
+    politiciansTestKeys.append(key)
+
+for key in politiciansResultsData.keys():
+    politiciansResultsKeys.append(key)
+
+politiciansResultsIterator = 0
+@app.route("/politicians_results", methods=['GET', 'POST'])
+def politiciansResults():
+    if(request.method == 'POST'):
+        global politiciansResultsIterator
+        politiciansResultsIterator += 1
+    return render_template("results.html", title = "politicians", testData = politiciansTestData, resultsData = politiciansResultsData, iterator = politiciansResultsIterator, testKey = politiciansTestKeys[politiciansResultsIterator])
+
+cricketersTestFile = open('Transformer Output/cricketers_test.json', 'r')
+cricketersResultsFile = open('Transformer Output/cricketers_matches.json', 'r')
+with open('Transformer Output/cricketers_test.json', 'r') as cricketersTestFile:
+    cricketersTestData = json.load(cricketersTestFile)
+with open('Transformer Output/cricketers_matches.json', 'r') as cricketersResultsFile:
+    cricketersResultsData = json.load(cricketersResultsFile)
+
+cricketersTestKeys = []
+cricketersResultsKeys = []
+
+for key in cricketersTestData.keys():
+    cricketersTestKeys.append(key)
+
+for key in cricketersResultsData.keys():
+    cricketersResultsKeys.append(key)
+
+cricketersResultsIterator = 0
+@app.route("/cricketers_results", methods=['GET', 'POST'])
+def cricketersResults():
+    if(request.method == 'POST'):
+        global cricketersResultsIterator
+        cricketersResultsIterator += 1
+    return render_template("results.html", title = "cricketers", testData = cricketersTestData, resultsData = cricketersResultsData, iterator = cricketersResultsIterator, testKey = cricketersTestKeys[cricketersResultsIterator])
+
+actorsTestFile = open('Transformer Output/actors_test.json', 'r')
+actorsResultsFile = open('Transformer Output/actors_matches.json', 'r')
+with open('Transformer Output/actors_test.json', 'r') as actorsTestFile:
+    actorsTestData = json.load(actorsTestFile)
+with open('Transformer Output/actors_matches.json', 'r') as actorsResultsFile:
+    actorsResultsData = json.load(actorsResultsFile)
+
+actorsTestKeys = []
+actorsResultsKeys = []
+
+for key in actorsTestData.keys():
+    actorsTestKeys.append(key)
+
+for key in actorsResultsData.keys():
+    actorsResultsKeys.append(key)
+
+actorsResultsIterator = 0
+@app.route("/actors_results", methods=['GET', 'POST'])
+def actorsResults():
+    if(request.method == 'POST'):
+        global actorsResultsIterator
+        actorsResultsIterator += 1
+    return render_template("results.html", title = "actors", testData = actorsTestData, resultsData = actorsResultsData, iterator = actorsResultsIterator, testKey = actorsTestKeys[actorsResultsIterator])
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
